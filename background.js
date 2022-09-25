6e7143dcb7fdf433483618234d36266c8e4d088f
toolsmax0 <xingshijichina@outlook.com>
2022-09-26 03:36:48 +0800

    persistency

--- background.js
+++ background.js
@@ -1,17 +1,19 @@
 const period = 360;
 const alarmName = "checkdeck";
+
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.alarms.onAlarm.addListener(checkDeck);
   chrome.alarms.create(alarmName, { when: 0, periodInMinutes: period });
 });
-chrome.runtime.onStartup.addListener(() => {
-  chrome.alarms.onAlarm.addListener(checkDeck);
-  report();
-});
+
+chrome.alarms.onAlarm.addListener(checkDeck);
+
+chrome.runtime.onStartup.addListener(report);
+
 function checkDeck() {
-  chrome.tabs.create({url:"deck.html"});
+  chrome.tabs.create({ url: "deck.html" });
   report();
 }
+
 function report() {
   chrome.alarms.get(alarmName, (alarm) => {
     var t = new Date(alarm.scheduledTime);
