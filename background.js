ae6cb9e389496a227a5c7d8839f01624ada181e5
toolsmax0 <xingshijichina@outlook.com>
2022-09-25 20:49:23 +0800

    simplify

--- background.js
+++ background.js
@@ -1,34 +1,20 @@
-const period = 1000 * 60 * 60 * 6;
+const period = 360;
 const alarmName = "checkdeck";
 chrome.runtime.onInstalled.addListener(() => {
   chrome.alarms.onAlarm.addListener(checkDeck);
-  var nexttime = 0;
-  chrome.storage.sync.set({ nexttime });
-  chrome.alarms.create(alarmName, { when: nexttime });
-  console.log("Alarm Created");
+  chrome.alarms.create(alarmName, { when: 0, periodInMinutes: period });
 });
 chrome.runtime.onStartup.addListener(() => {
-  console.log("alive");
   chrome.alarms.onAlarm.addListener(checkDeck);
+  report();
+});
+function checkDeck() {
+  chrome.tabs.create({url:"deck.html"});
+  report();
+}
+function report() {
   chrome.alarms.get(alarmName, (alarm) => {
     var t = new Date(alarm.scheduledTime);
     console.log("Next trigger time: " + t);
-    // console.log(alarm);
-    if (!alarm) {
-      chrome.storage.sync.get("nexttime", ({ nexttime }) => {
-        chrome.alarms.create(alarmName, { when: nexttime });
-        var t = new Date(nexttime);
-        console.log("Next trigger time: " + t);
-      });
-    }
   });
-});
-function checkDeck() {
-  chrome.tabs.create({ url: "deck.html" });
-  console.log("Deck Checked");
-  var nexttime = Date.now() + period;
-  chrome.alarms.create(alarmName, { when: nexttime });
-  chrome.storage.sync.set({ nexttime });
-  var t = new Date(nexttime);
-  console.log("Next trigger time: " + t);
 }
